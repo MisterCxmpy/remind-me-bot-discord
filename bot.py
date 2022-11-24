2af5b76ea5d5e12554024b2593eec231ebd50305
MisterCxmpy <cosgunhussein@gmail.com>
2022-11-24 14:37:58 +0000

    Bot outputs (year/month/day + time) which user inputs

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 import os
 import json
 import sys
+from datetime import datetime, timezone, timedelta
 
 from discord.ext import commands, tasks
 from discord.ext.commands import Bot, Context
 import discord
+
+year = 365
+month = 30
 
 if not os.path.isfile("config.json"):
     sys.exit("'config.json' not found! Please add it and try again.")
@@ -24,8 +28,35 @@ async def on_ready():
     print("----------------------")
 
 @client.command()
-async def hello(ctx):
-    username = str(ctx.author).split('#')[0]
-    await ctx.send(username)
+async def remind(ctx, time, time_period):
+
+    if time_period not in ["seconds", "minutes", "hours", "days", "weeks", "months", "years"]:
+        return
+
+    current_time = datetime.now(timezone.utc)
+
+    if time_period == "seconds":
+        time_change = timedelta(seconds=int(time))
+    elif time_period == "minutes":
+        time_change = timedelta(minutes=int(time))
+    elif time_period == "hours":
+        time_change = timedelta(hours=int(time))
+    elif time_period == "days":
+        time_change = timedelta(days=int(time))
+    elif time_period == "weeks":
+        time_change = timedelta(weeks=int(time))
+    elif time_period == "months":
+        time_change = timedelta(days=(month * int(time)))
+    elif time_period == "years":
+        time_change = timedelta(days=(year * int(time)))
+    else:
+        return
+    
+    remind_time = current_time + time_change
+
+    print('Current UTC Time:', current_time + time_change)
+
+    await ctx.send("Messaging you on " + str(remind_time)[:19] + " UTC to remind you of this comment.")
+    
 
 client.run(config["token"])
